3988778c4888353aa07a5b44472ead196df02ece
Jason Sarrio <jsarrio16@gmail.com>
2020-07-21 16:26:12 -0500

    Working Sprint used Yarn insted of NPM

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -1,21 +1,41 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
 
+import React, { useState, useEffect } from "react";
 import Bubbles from "./Bubbles";
 import ColorList from "./ColorList";
+
+
+
+//Imports
+
+
+import { axiosWithAuth } from '../utils/axiosWithAuth.js';
+
+
 
 const BubblePage = () => {
   const [colorList, setColorList] = useState([]);
   // fetch your colors data from the server when the component mounts
   // set that data to the colorList state property
+  const getColors = () => (
+
+    axiosWithAuth()
+      .get('/api/colors')
+      .then(res => setColorList(res.data))
+      .catch(err => console.log(err))
+
+  )
+
+  useEffect(() => { getColors() }, []);
 
   return (
     <>
-      <ColorList colors={colorList} updateColors={setColorList} />
+      <ColorList 
+        colors={colorList} updateColors={setColorList}
+        getColors={getColors}
+        />
       <Bubbles colors={colorList} />
     </>
   );
 };
-
 
 export default BubblePage;
